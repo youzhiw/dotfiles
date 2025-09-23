1b943d2a310e94530652c2797d3016ab8837072e
Youzhi Wang <yw4196@columbia.edu>
2025-09-23 15:01:34 -0400

    updates colors for catputtcin, change tabstop to 8, added clangd lsp config

--- nvim/.config/nvim/lua/youzhi/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/youzhi/plugins/lsp-config.lua
@@ -1,40 +1,58 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "ts_ls", "jdtls" }
-            })
-        end
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local lspconfig = require("lspconfig")
-            -- -- Python LSP
-            -- lspconfig.pyright.setup({})
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
+			-- -- Python LSP
+			-- lspconfig.pyright.setup({})
 
-            -- Lua LSP
-            lspconfig.lua_ls.setup({})
+			-- Lua LSP
+			lspconfig.lua_ls.setup({})
 
-            lspconfig.jdtls.setup({})
+			-- lspconfig.jdtls.setup({})
 
-            -- TypeScript and JavaScript LSP
-            lspconfig.ts_ls.setup({
-                on_attach = function(client, bufnr)
-                end
-            })
-
-            -- Keybindings for LSP actions
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-            vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, {})
-        end
-    }
+			-- C / C++: clangd
+			lspconfig.clangd.setup({
+				-- mason-lspconfig will point to Mason's clangd automatically if installed there
+				cmd = {
+					"clangd",
+					"--background-index",
+					"--clang-tidy", -- run clang-tidy checks
+					"--completion-style=detailed",
+					"--header-insertion=iwyu", -- smarter include insertion
+					"--fallback-style=LLVM", -- fallback formatting style
+				},
+				-- Uncomment if you use nvim-cmp capabilities
+				-- capabilities = require("cmp_nvim_lsp").default_capabilities(),
+				on_attach = function(client, bufnr)
+					-- optional: format on save for C/C++
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format({ async = false })
+						end,
+					})
+				end,
+			})
+			-- Keybindings for LSP actions
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostics" })
+		end,
+	},
 }
